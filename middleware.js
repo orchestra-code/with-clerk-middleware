bcc860dcb42465b59d6aa131257e654a832a9ad5
Jim Wrubel <jwrubel@gmail.com>
2023-06-14 20:13:30 -0400

    customization to show issue with middleware and dots in slug

--- middleware.js
+++ middleware.js
@@ -1,9 +1,46 @@
+import { NextResponse } from "next/server";
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  // Set the paths that don't require the user to be signed in
-  // Sign in and sign up pages are already made public by Clerk
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/www(.*)",
+    "/app/sign-up",
+    "/app/sign-in",
+  ],
+  beforeAuth(req, evt) {
+    console.log(`we are before auth, req.nextURL is ${req.nextUrl}`)
+  },
+  afterAuth(auth, req, evt) {
+    const url = req.nextUrl;
+    console.log(`app route is ${url}`)
+
+    const { pathname } = req.nextUrl;
+
+    const hostname = req.headers.get("host");
+
+    let currentHost = hostname
+    .toLowerCase()
+    .replace(`.localhost:3000`, "")
+    .toLowerCase();
+
+    // rewrites for app pages
+    if (!pathname.startsWith("/api")) {
+      if (currentHost === "app") {
+        // handle users who aren't authenticated
+        console.log(`app route is ${url.pathname}`)
+
+        url.pathname = `/app${url.pathname}`;
+
+      } else {
+        url.pathname = `/www${url.pathname}`;
+
+      }
+
+      return NextResponse.rewrite(url);
+    }
+  }
 });
 
-export const config = { matcher: "/((?!_next/image|_next/static|favicon.ico|.*.svg).*)" };
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)","/","/(api|trpc)(.*)"],
+};
